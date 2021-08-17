db37e782ac5bb76fe10e8b589dbf6e9420e78465
skynocover <skynocover@Gmail.com>
2021-08-17 14:47:36 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/version', (req, res) => {
+  res.json({ env: process.env.NODE_ENV, version: process.env.VERSION });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(new Date(), `env: ${process.env.NODE_ENV}`);
+  console.log(new Date(), `version: ${process.env.VERSION}`);
+  console.log(new Date(), `server listening on ${process.env.PORT}`);
+});
